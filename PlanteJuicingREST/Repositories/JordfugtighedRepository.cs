341169db6d658b2c7120a1db4a5dec7986b55f91
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-05-05 11:06:14 +0200

    Interface for jordfugtighed repository og test af repository

---
+++ PlanteJuicingREST/Repositories/JordfugtighedRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using PlanteJuicingREST.Models;
+using PlanteJuicingREST.Interface;
+
+namespace PlanteJuicingREST.Repositories
+{
+    public class JordfugtighedRepository : IJordfugtighedRepository
+    {
+        private List<Jordfugtighed> _jordfugtighedList = new List<Jordfugtighed>();
+        public IEnumerable<Jordfugtighed> GetAllJordfugtighed()
+        {
+            return _jordfugtighedList;
+        }
+        public Jordfugtighed Add(Jordfugtighed jordfugtighed)
+        {
+            _jordfugtighedList.Add(jordfugtighed);
+            return jordfugtighed;
+        }
+    }
+}
