27596a88ac6a6ea258354680e59d40c13f921e68
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-05-05 10:52:09 +0200

    model class

---
+++ PlanteJuicingREST/Models/Jordfugtighed.cs
@@ -0,0 +1,36 @@
+﻿namespace PlanteJuicingREST.Models
+{
+    public class Jordfugtighed
+    {
+        private int _id;
+        private int _jordfugtighed;
+
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("id must be higher than 0");
+                _id = value;
+            }
+        }
+        public int JordfugtighedValue
+        {
+            get => _jordfugtighed;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("jordfugtighed must be higher than 0");
+
+                _jordfugtighed = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
