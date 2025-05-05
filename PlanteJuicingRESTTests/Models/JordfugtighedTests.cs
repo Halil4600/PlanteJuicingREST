a70a40378d3a85d43b3694bbb98c3b5d1ccec2d0
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-05-05 10:47:14 +0200

    Test af model claas for fugtihed

---
+++ PlanteJuicingRESTTests/Models/JordfugtighedTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanteJuicingREST.Models;
+using System;
+
+namespace PlanteJuicingREST.Models.Tests
+{
+    [TestClass]
+    public class JordfugtighedTests
+    {
+        [TestMethod]
+        public void IdTest()
+        {
+            // Arrange
+            Jordfugtighed jordfugtighed = new Jordfugtighed();
+
+            // Act & Assert
+            jordfugtighed.Id = 1;
+            Assert.AreEqual(1, jordfugtighed.Id);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => jordfugtighed.Id = -1);
+        }
+
+        [TestMethod]
+        public void JordfugtighedValueTest()
+        {
+            // Arrange
+            Jordfugtighed jordfugtighed = new Jordfugtighed();
+
+            // Act & Assert
+            jordfugtighed.JordfugtighedValue = 50;
+            Assert.AreEqual(50, jordfugtighed.JordfugtighedValue);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => jordfugtighed.JordfugtighedValue = -10);
+        }
+
+    }
+}
